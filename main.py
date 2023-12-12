704e4a6bf0e4f64fe12f85729b212434afd5402e
Antoine Chapelant <achapelant@gmail.com>
2023-12-12 16:15:19 +0100

    change snake position

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ snake_position = [(10, 5), (10, 6), (10, 7)]
 
 
 def display_checkerboard():
+    screen.fill(SCREEN_COLOR)
     k, l = int(SCREEN_HEIGHT / TILES_SIZE), int(SCREEN_WIDTH / TILES_SIZE)
     for i in range(k):
         for j in range(l):
@@ -24,14 +25,22 @@ def display_checkerboard():
                 pygame.draw.rect(screen, TILES_COLOR, rect)
 
 
-def draw_snake(snake_position):
+def draw_snake():
     for i, j in snake_position:
         x, y = i * TILES_SIZE, j * TILES_SIZE
         rect = pygame.Rect(y, x, TILES_SIZE, TILES_SIZE)
         pygame.draw.rect(screen, SNAKE_COLOR, rect)
 
 
+def change_snake_position():
+    snake_position.append(
+        (snake_position[-1][0] + direction[0], snake_position[-1][1] + direction[1])
+    )
+    snake_position.pop(0)
+
+
 status = 1  # 1 when running, 0 to stop
+direction = (0, 1)
 
 pygame.init()
 
@@ -48,12 +57,24 @@ while status:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:  # quit the game
                 status = 0
+
+            # DIRECTION
+            if event.key == pygame.K_UP:
+                direction = (-1, 0)
+            if event.key == pygame.K_DOWN:
+                direction = (1, 0)
+            if event.key == pygame.K_LEFT:
+                direction = (0, -1)
+            if event.key == pygame.K_RIGHT:
+                direction = (0, 1)
+
         if event.type == pygame.QUIT:  # quit the game
             status = 0
 
-    screen.fill(SCREEN_COLOR)
     display_checkerboard()
-    draw_snake(snake_position)
+    change_snake_position()
+    draw_snake()
+
     pygame.display.update()
 
 print(f"{status=}")
