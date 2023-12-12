70455e50987168d7e7e569034fec5dbe95020ab9
Antoine Chapelant <achapelant@gmail.com>
2023-12-12 14:50:20 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(1)
+
+    for event in pygame.event.get():
+        pass
+
+    screen.fill((255, 255, 255))
+
+    pygame.display.update()
